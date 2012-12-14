7f6cd5e0cd9d0b39591507393ff1ef01346d2154
evan1026 <kkrockme4@gmail.com>
2012-12-13 21:58:35 -0500

    Added stuff to EncoderDrive (git was being mean last commit)

--- src/storm2013/preseason/commands/EncoderDrive.java
+++ src/storm2013/preseason/commands/EncoderDrive.java
@@ -10,22 +10,84 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  * @author Evan Allan
  */
+
 public class EncoderDrive extends Command {
 
-    protected void initialize() {
+    private static final double DRIVE_SPEED = 1; //TODO find the number this should be.
+    
+    private double leftGoal_, rightGoal_, leftDist_, rightDist_;
+    private AccelerateToward accel_ = new AccelerateToward();
+    
+    public EncoderDrive(){}
+    public EncoderDrive(double leftGoal, double rightGoal, double leftDist, double rightDist){ 
+        leftGoal_ = leftGoal;
+        rightGoal_ = rightGoal;
+        leftDist_ = leftDist; //Not really necessary but hey, you might want it
+        rightDist_ = leftDist;//This too
+        if (leftGoal_ > leftDist_) {accel_.setTargetLeft(DRIVE_SPEED);}
+        else if (leftGoal_ < 0) {accel_.setTargetLeft(-DRIVE_SPEED);}
+        if (rightGoal_ > rightDist_) {accel_.setTargetRight(DRIVE_SPEED);}
+        else if (rightGoal_ < 0) {accel_.setTargetRight(-DRIVE_SPEED);}
     }
-
-    protected void execute() {
+    
+    public void setLeftDist_(double leftDist) {
+        leftDist_ = leftDist;
     }
-
-    protected boolean isFinished() {
-        return false;
+    public void setRightDist_(double rightDist) {
+        rightDist_ = rightDist;
     }
-
-    protected void end() {
+    public void setLeftGoal_(double leftGoal){
+        leftGoal_ = leftGoal;
     }
-
-    protected void interrupted() {
+    public void setRightGoal_(double rightGoal){
+        rightGoal_ = rightGoal;
+    }
+    public double getLeftGoal() {
+        return leftGoal_;
+    }
+    public double getRightGoal() {
+        return rightGoal_;
+    }
+    public double getLeftDist() {
+        return leftDist_;
+    }
+    public double getRightDist() {
+        return rightDist_;
+    }
+    
+    public void initialize() {
+        accel_.start();
+    }
+    public void execute() {
+        //TODO learn about how to get encoder values and use that to find the distance
+        //TODO also, make some PID-ish kinda thing, right now it'll bounce back and forth until it reaches the needed value
+        if (leftDist_ == leftGoal_){
+            accel_.setTargetLeft(0); 
+        }
+        else if(leftDist_ > leftGoal_ && accel_.getTargetLeft() != -DRIVE_SPEED){
+            accel_.setTargetLeft(-DRIVE_SPEED);
+        }
+        else if(leftDist_ < leftGoal_ && accel_.getTargetLeft() != DRIVE_SPEED){
+            accel_.setTargetLeft(DRIVE_SPEED);
+        }
+        if (rightDist_ == rightGoal_){
+            accel_.setTargetRight(0);
+        }
+        else if(rightDist_ > rightGoal_ && accel_.getTargetRight() != -DRIVE_SPEED){
+            accel_.setTargetRight(-DRIVE_SPEED);
+        }
+        else if(rightDist_ < rightGoal_ && accel_.getTargetRight() != DRIVE_SPEED){
+            accel_.setTargetRight(DRIVE_SPEED);
+        } 
+    }
+    public boolean isFinished() {
+        return !accel_.isRunning();
+    }
+    public void end() {
+        accel_.cancel();
+    }
+    public void interrupted() {
+        end();
     }
     
 }
