bb07260167cdcb4ad919c65c0f11a72083f7cf64
evan1026 <kkrockme4@gmail.com>
2012-12-13 21:54:38 -0500

    Added EncoderDrive

---
+++ src/storm2013/preseason/commands/EncoderDrive.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storm2013.preseason.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author Evan Allan
+ */
+public class EncoderDrive extends Command {
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+    
+}
